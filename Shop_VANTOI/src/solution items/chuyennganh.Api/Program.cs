75f14e7f25c7f53913d61c4e7f2cbe8d35bee02d
Tới Văn <tovantoi2003@gmail.com>
2024-12-04 23:09:26 +0700

    fix code

--- Shop_VANTOI/src/solution items/chuyennganh.Api/Program.cs
+++ Shop_VANTOI/src/solution items/chuyennganh.Api/Program.cs
@@ -37,26 +37,6 @@ builder.Services.AddSwaggerGen();
 
 // Add application services
 builder.Services.AddApplicationServices();
-var jwtSettings = builder.Configuration.GetSection("JwtSettings");
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings["Issuer"],
-        ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
-    };
-});
-
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
